d4a00a7f0a280ab31aa0befd9995472971d9a353
Nguyenductuan-dev <ndtuan96nb@gmail.com>
2022-04-26 01:20:47 +0700

    admin
    update feature and product1

--- .history/assets/js/main_20220425215029.js
+++ .history/assets/js/main_20220425215029.js
@@ -2,7 +2,8 @@
 function scrollheader() {
     let header = document.getElementById('header');
     if (this.scrollY >= 200) {
-        header.classList.add('scroll-header');
+        console.log(this);
+        header.classList.add('scroll-header')
     }
     else {
         header.classList.remove('scroll-header');
