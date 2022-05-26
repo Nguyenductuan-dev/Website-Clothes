09d75bfafee60da265164f6568d0902060eb3cfc
Nguyenductuan-dev <72592265+Nguyenductuan-dev@users.noreply.github.com>
2022-05-26 15:25:10 +0700

    admin
    update breakpoint

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,19 @@ function scrollheader() {
     }
 }
 window.addEventListener('scroll', scrollheader);
+
+/* ================ SHOW/HIDDEN MENU MOBILE ================*/
+let iconMobile = document.getElementById('icon-navbar');
+let close = document.getElementById('close');
+let navbar = document.getElementById('navbar');
+
+if(iconMobile) {
+    iconMobile.addEventListener('click', () => {
+        navbar.classList.add('active');
+    })
+}
+if(close) {
+    close.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    })
+}
